e647f0dc3daf4b2d56f754b56c736e42932cc9ba
Omar Faruk <162047444+ofbabu@users.noreply.github.com>
2024-05-23 17:57:26 +0600

    Add files via upload

---
+++ JavaScript/class ES6/index.js
@@ -0,0 +1,55 @@
+
+/*
+// let age = 40 ; error- let ke redeclar kora jay na but resong kora jabe  atta akta block elements
+let age = 20
+age = 22
+console.log(age)*/
+
+ /* var age =20
+  var age = 40
+  console.log(age)*/
+
+//   function sum(){
+//     let num1 = 10;
+//     let num2 = 20;
+//     const total = num1 + num2;
+//     return total;
+//   }
+//   console.log(sum())
+
+//   console.log(num1) / eita num1 is not defined karon let hosse block elements ja blocker mossei frokas kore
+
+/*for (let i = 0; i<5; i++){
+    // console.log("inside Loop"+1)
+}
+console.log("inside Loop"+i)*/
+
+// for (var i = 0; i<5; i++){
+//     // console.log("inside Loop"+1)
+// }
+// console.log("inside Loop"+i)
+
+// const a = 10
+// // const a = 50
+// // a=20
+// console.log(a)
+
+// const obj = { name: 'babu'}
+// // obj = {age:40} / resign hoyese tai error khaise
+//  obj.name = 'omar faruk'
+// console.log(obj.name)
+
+
+// //--------deffole function-------------
+        // // es5 er jonno
+        // function say(message){
+        //     message = typeof message !== "undefined"? message: 'say hi'
+        //     console.log(message)
+        // }
+        // say('omar babu')
+
+        // ---es6
+// function say(message = 'Type showing message'){
+//     console.log(message);
+// }
+// say('omar faruk')
